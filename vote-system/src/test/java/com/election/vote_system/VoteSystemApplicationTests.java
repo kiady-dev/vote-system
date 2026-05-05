803afef958191189922df9f8a723bea6a6f7805d
RAHAMELOSON Kiady Ifaliana <kiadyrahameloson@gmail.com>
2026-05-05 11:02:58 +0300

    mise en place springboot
    ok

---
+++ vote-system/src/test/java/com/election/vote_system/VoteSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.election.vote_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
